92081e8fcd1ff649282475346de3035a486d3688
TQA <trinhquocanh94@gmail.com>
2019-01-30 23:31:01 +0100

    Solve several exercises.

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,18 @@
+from string import ascii_uppercase, digits
+from random import choice
+
+
 class Robot(object):
     def __init__(self):
-        pass
+        self.name = self.generate_name()
+        self.used_name = [self.name]
+
+    def reset(self):
+        while self.name in self.generate_name():
+            self.name = self.generate_name()
+            self.used_name.append(self.name)
+
+    def generate_name(self):
+        return ''.join([choice(ascii_uppercase)
+                        for i in range(2)] +
+                       [choice(digits) for i in range(3)])
--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,3 +1,27 @@
+year_seconds = 31557600
+
+year_map = {
+    'earth': 1,
+    'mercury': 0.2408467,
+    'venus': 0.61519726,
+    'mars': 1.8808158,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132,
+}
+
+
 class SpaceAge(object):
     def __init__(self, seconds):
-        pass
+        self.seconds = seconds
+
+
+def on_planet(r):
+    def inner(self):
+        return round(self.seconds / year_seconds / r, 2)
+    return inner
+
+
+for k, v in year_map.items():
+    setattr(SpaceAge, "on_{}".format(k), on_planet(v))
