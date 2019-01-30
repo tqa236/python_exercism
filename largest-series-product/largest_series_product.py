e781c8192cac3e3d28fb3ac29d097d38cae6263e
TQA <trinhquocanh94@gmail.com>
2019-01-30 22:25:51 +0100

    Solve largest series product exercise.

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,11 @@
+from operator import mul
+from functools import reduce
+
+
 def largest_product(series, size):
-    pass
+    if size == 0:
+        return 1
+    if size < 0:
+        raise ValueError("Invalid inputs")
+    return max([reduce(mul, [int(x) for x in series[i:i+size]], 1)
+                for i in range(len(series) - size + 1)])
