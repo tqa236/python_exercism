bf5778d12b1455b7117e7341fd11c9cc348897ba
TQA <trinhquocanh94@gmail.com>
2019-01-31 12:39:38 +0100

    Solve some exercises

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,13 @@
 def proteins(strand):
-    pass
+    codons = {"AUG": "Methionine",
+              "UUU": "Phenylalanine", "UUC": "Phenylalanine",
+              "UUA": "Leucine", "UUG": "Leucine",
+              "UCU": "Serine", "UCC": "Serine",
+              "UCA": "Serine", "UCG": "Serine",
+              "UAU": "Tyrosine", "UAC": "Tyrosine",
+              "UGU": "Cysteine", "UGC": "Cysteine",
+              "UGG": "Tryptophan",
+              "UAA": "STOP", "UAG": "STOP", "UGA": "STOP"}
+    proteins = [codons[strand[i:i+3]] for i in range(0, len(strand), 3)]
+    return proteins[:None if "STOP" not in proteins
+                    else proteins.index("STOP")]
--- rational-numbers/rational_numbers.py
+++ rational-numbers/rational_numbers.py
@@ -1,10 +1,16 @@
 from __future__ import division
+from math import gcd
 
 
 class Rational(object):
     def __init__(self, numer, denom):
-        self.numer = None
-        self.denom = None
+        if denom == 0:
+            raise ValueError("Denominator cannot be 0")
+        if denom < 0:
+            numer = numer * -1
+        gcd_ = gcd(numer, denom)
+        self.numer = numer//gcd_
+        self.denom = abs(denom//gcd_)
 
     def __eq__(self, other):
         return self.numer == other.numer and self.denom == other.denom
@@ -13,22 +19,24 @@ class Rational(object):
         return '{}/{}'.format(self.numer, self.denom)
 
     def __add__(self, other):
-        pass
+        return Rational(self.numer * other.denom + self.denom * other.numer,
+                        self.denom * other.denom)
 
     def __sub__(self, other):
-        pass
+        return Rational(self.numer * other.denom - self.denom * other.numer,
+                        self.denom * other.denom)
 
     def __mul__(self, other):
-        pass
+        return Rational(self.numer*other.numer, self.denom*other.denom)
 
     def __truediv__(self, other):
-        pass
+        return Rational(self.numer*other.denom, self.denom*other.numer)
 
     def __abs__(self):
-        pass
+        return Rational(abs(self.numer), self.denom)
 
     def __pow__(self, power):
-        pass
+        return Rational(self.numer**power, self.denom**power)
 
     def __rpow__(self, base):
-        pass
+        return base**(self.numer/self.denom)
