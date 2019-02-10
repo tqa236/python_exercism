e05d373db6347aba4228eb43dbb9974511b2bcb7
TQA <trinhquocanh94@gmail.com>
2019-02-10 21:44:04 +0100

    Add travis

--- sgf-parsing/sgf_parsing.py
+++ sgf-parsing/sgf_parsing.py
@@ -32,17 +32,17 @@ def parse(input_string):
         raise ValueError("Invalid input")
     if input_string == "(;)":
         return SgfTree()
-    nodes = list(filter(None, input_string[1:-1].split(";")))
-    print("nodes = ", nodes)
+    # nodes = list(filter(None, input_string[1:-1].split(";")))
+    # print("nodes = ", nodes)
     tree = {}
-    for i in nodes:
-        if "[" not in i:
-            raise ValueError("Node must have properties")
-        key = i.split("[")[0]
-        if key != key.upper():
-            raise ValueError("Keys must be all capital")
-        tree[key] = [i.split("[")[1][:-1]]
-    print()
-    print(tree)
+    # for i in nodes:
+    #     if "[" not in i:
+    #         raise ValueError("Node must have properties")
+    #     key = i.split("[")[0]
+    #     if key != key.upper():
+    #         raise ValueError("Keys must be all capital")
+    #     tree[key] = [i.split("[")[1][:-1]]
+    # print()
+    # print(tree)
 
     return SgfTree(properties=tree)
