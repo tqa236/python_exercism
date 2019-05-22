5486c3ee9efa92a5eb6d37d5f4546ae80c052241
TQA <trinhquocanh94@gmail.com>
2019-05-22 22:09:50 +0200

    Solve flatten array

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,2 +1,9 @@
+"""Flatten nested array."""
+from collections import Iterable
+
+
 def flatten(iterable):
-    pass
+    """Flatten nested array."""
+    if isinstance(iterable, Iterable) and not isinstance(iterable, str):
+        return [a for i in iterable for a in flatten(i) if a is not None]
+    return [iterable]
