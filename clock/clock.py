948b69e593bdd6b15527fb6ce176b8f9cc491b71
TQA <trinhquocanh94@gmail.com>
2019-05-25 19:08:07 +0200

    Solve Clock

--- clock/clock.py
+++ clock/clock.py
@@ -1,15 +1,26 @@
+"""A simple Clock class."""
+
+
 class Clock(object):
+    """A simple Clock class."""
+
     def __init__(self, hour, minute):
-        pass
+        """Initialize a Clock object with the correct hour and minute."""
+        self.hour = (hour + minute // 60) % 24
+        self.minute = minute % 60
 
     def __repr__(self):
-        pass
+        """Display time with format hh:mm."""
+        return f"{self.hour:02d}:{self.minute:02d}"
 
     def __eq__(self, other):
-        pass
+        """Compare time of two clocks."""
+        return self.hour == other.hour and self.minute == other.minute
 
     def __add__(self, minutes):
-        pass
+        """Add time of a clock."""
+        return Clock(self.hour, self.minute + minutes)
 
     def __sub__(self, minutes):
-        pass
+        """Substract time from a clock."""
+        return Clock(self.hour, self.minute - minutes)
