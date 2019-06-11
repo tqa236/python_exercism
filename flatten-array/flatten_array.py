145ec64c028538147510f0d236792be11fb5e6f7
TQA <trinhquocanh94@gmail.com>
2019-06-11 21:36:55 +0200

    Test type hint

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,8 +1,9 @@
 """Flatten nested array."""
 from collections import Iterable
+from typing import Any, List, Union
 
 
-def flatten(iterable):
+def flatten(iterable: Any) -> Union[List[str], List[int], List[None]]:
     """Flatten nested array."""
     if isinstance(iterable, Iterable) and not isinstance(iterable, str):
         return [a for i in iterable for a in flatten(i) if a is not None]
