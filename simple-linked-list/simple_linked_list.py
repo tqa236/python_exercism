cc4f21f3909c303a9ed010c50dee51f129376fd1
TQA <trinhquocanh94@gmail.com>
2019-07-23 20:50:23 +0200

    Half way through simple linked list

--- simple-linked-list/simple_linked_list.py
+++ simple-linked-list/simple_linked_list.py
@@ -1,32 +1,53 @@
 class Node(object):
     def __init__(self, value):
-        pass
+        self.node_value = value
+        self.next_node = None
 
     def value(self):
-        pass
+        return self.node_value
 
     def next(self):
-        pass
+        return self.next_node
 
 
 class LinkedList(object):
     def __init__(self, values=[]):
-        pass
+        self.head_node = None
+        self.length = 0
+        for value in values:
+            self.push(value)
+        self.length = len(values)
+        self.index = self.length
 
     def __len__(self):
-        pass
+        return self.length
+
+    def __iter__(self):
+        return (self.pop() for i in range(self.length))
 
     def head(self):
-        pass
+        if self.__len__() == 0:
+            raise EmptyListException("Empty linked list.")
+        return self.head_node
 
     def push(self, value):
-        pass
+        node = Node(value)
+        node.next_node = self.head_node
+        self.head_node = node
+        self.length = self.length + 1
 
     def pop(self):
-        pass
+        if self.__len__() == 0:
+            raise EmptyListException("Empty linked list.")
+        self.length = self.length - 1
+        value = self.head().value()
+        self.head_node = self.head_node.next()
+        return value
 
     def reversed(self):
-        pass
+        if self.head_node is None:
+            return self
+        return self
 
 
 class EmptyListException(Exception):
