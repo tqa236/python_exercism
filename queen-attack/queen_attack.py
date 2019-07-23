350414d37be25cc369f738ad71bb4256192c44a5
TQA <trinhquocanh94@gmail.com>
2019-07-23 19:54:55 +0200

    Solve Queen Attack

--- queen-attack/queen_attack.py
+++ queen-attack/queen_attack.py
@@ -1,6 +1,25 @@
+"""Decide if 2 queens can attack each other."""
+
+
 class Queen(object):
+    """A simple queen object."""
+
     def __init__(self, row, column):
-        pass
+        """Initialize the position of the queen."""
+        if min(row, column) >= 0 and max(row, column) < 8:
+            self.row = row
+            self.column = column
+        else:
+            raise ValueError("Unexpected Exception")
 
     def can_attack(self, another_queen):
-        pass
+        """Decide if this queen can attack another queen."""
+        if (self.row, self.column) == (another_queen.row, another_queen.column):
+            raise ValueError("Unexpected Exception")
+        if self.row == another_queen.row or self.column == another_queen.column:
+            return True
+        if self.row + self.column == another_queen.row + another_queen.column:
+            return True
+        if abs(self.row - self.column) == abs(another_queen.row - another_queen.column):
+            return True
+        return False
