e63d8847d60887cd85fe41cc720bc0fcb6ce1269
TQA <trinhquocanh94@gmail.com>
2019-07-27 21:00:01 +0200

    Solve roman numerals

--- all-your-base/all_your_base.py
+++ all-your-base/all_your_base.py
@@ -1,3 +1,5 @@
 from typing import List
+
+
 def rebase(input_base: int, digits: List[int], output_base: int) -> None:
     pass
--- roman-numerals/roman_numerals.py
+++ roman-numerals/roman_numerals.py
@@ -1,2 +1,15 @@
+"""Convert a number to roman numerals."""
+
+
 def roman(number: int) -> None:
-    pass
+    """Convert a number to roman numerals."""
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    roman_number = ""
+    i = 0
+    while number > 0:
+        for _ in range(number // val[i]):
+            roman_number = roman_number + syb[i]
+            number = number - val[i]
+        i += 1
+    return roman_number
