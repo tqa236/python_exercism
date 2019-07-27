67b296e25522e807c94be9d4132b4dc4b5400515
TQA <trinhquocanh94@gmail.com>
2019-07-27 16:20:43 +0200

    Atbash cipher

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,9 +1,26 @@
+"""Encode and decode text with atbash cipher."""
 from typing import Optional
+import re
+import string
 
 
 def encode(plain_text: str) -> None:
-    pass
+    """Encode."""
+    pattern = re.compile(r"[\W_]+", re.UNICODE)
+    clean_text = pattern.sub("", plain_text).lower()
+    ciphered_text = decode(clean_text)
+    ciphered_text = " ".join(
+        [ciphered_text[i : i + 5] for i in range(0, len(ciphered_text), 5)]
+    )
+    return ciphered_text
 
 
 def decode(ciphered_text: Optional[str]) -> None:
-    pass
+    """Decode."""
+    reversed_alphabet = string.ascii_lowercase[::-1]
+    return "".join(
+        reversed_alphabet[string.ascii_lowercase.index(char)]
+        if char.isalpha()
+        else char
+        for char in ciphered_text.replace(" ", "")
+    )
