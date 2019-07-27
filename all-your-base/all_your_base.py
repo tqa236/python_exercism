ca62bdd8ba9206145439681ba920dc8939e5cd82
TQA <trinhquocanh94@gmail.com>
2019-07-27 21:28:16 +0200

    Solve all your base

--- all-your-base/all_your_base.py
+++ all-your-base/all_your_base.py
@@ -1,5 +1,28 @@
+"""Convert numbers in 2 bases."""
 from typing import List
 
 
 def rebase(input_base: int, digits: List[int], output_base: int) -> None:
-    pass
+    """Convert numbers in 2 bases."""
+    if input_base <= 1 or output_base <= 1:
+        raise ValueError("Base must be a integer bigger than 1.")
+    if digits:
+        if min(digits) < 0:
+            raise ValueError("Negative digit is not allowed.")
+        if max(digits) >= input_base:
+            raise ValueError("Digit exists in input_base.")
+    return dec2base(base2dec(input_base, digits), output_base)[::-1]
+
+
+def base2dec(input_base, digits):
+    """Convert numbers to base 10."""
+    if not digits:
+        return 0
+    return input_base * base2dec(input_base, digits[:-1]) + digits[-1]
+
+
+def dec2base(number, output_base):
+    """Convert numbers from base 10 (reversed order)."""
+    if not number:
+        return []
+    return [number % output_base] + dec2base(number // output_base, output_base)
