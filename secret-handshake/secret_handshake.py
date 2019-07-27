a0dad03c37b33b7ffe93e5f65d7c240412f0d81f
TQA <trinhquocanh94@gmail.com>
2019-07-27 21:50:22 +0200

    Solve secret handshape

--- secret-handshake/secret_handshake.py
+++ secret-handshake/secret_handshake.py
@@ -5,9 +5,17 @@ ACTIONS = ["wink", "double blink", "close your eyes", "jump"]
 
 def handshake(code: int) -> str:
     """Decode."""
-    return ACTIONS[code & 31]
+    actions = [action for key, action in enumerate(ACTIONS) if code & 2 ** key > 0]
+    if code >= 16:
+        actions = actions[::-1]
+    return actions
 
 
 def secret_code(actions):
     """Encode."""
-    return 0
+    indices = [ACTIONS.index(action) for action in actions]
+    if indices[0] <= indices[-1]:
+        offset = 0
+    else:
+        offset = 16
+    return offset + sum(2 ** index for index in indices)
