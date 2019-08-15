6701000a23074b83f9f515090e3de5bfe991c5c5
TQA <trinhquocanh94@gmail.com>
2019-08-15 23:24:03 +0200

    Solve dot dsl

--- dot-dsl/dot_dsl.py
+++ dot-dsl/dot_dsl.py
@@ -1,3 +1,4 @@
+"""A simple Domain Specific Language."""
 from typing import Any
 
 NODE, EDGE, ATTR = range(3)
@@ -28,4 +29,25 @@ class Edge(object):
 
 class Graph(object):
     def __init__(self, data: Any = []) -> None:
-        pass
+        self.nodes = []
+        self.edges = []
+        self.attrs = {}
+        if not isinstance(data, list):
+            raise TypeError("Type not known.")
+        for datum in data:
+            if len(datum) <= 1:
+                raise TypeError("Malformed graph item.")
+            if datum[0] == NODE:
+                if len(datum) != 3:
+                    raise ValueError("Value not good.")
+                self.nodes.append(Node(datum[1], datum[2]))
+            elif datum[0] == EDGE:
+                if len(datum) != 4:
+                    raise ValueError("Value not good.")
+                self.edges.append(Edge(datum[1], datum[2], datum[3]))
+            elif datum[0] == ATTR:
+                if len(datum) != 3:
+                    raise ValueError("Value not good.")
+                self.attrs[datum[1]] = datum[2]
+            else:
+                raise ValueError("Value not good.")
