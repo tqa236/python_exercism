31de870bd764f7aff1bc9e95d1894ad337c00885
TQA <trinhquocanh94@gmail.com>
2019-08-18 11:47:34 +0200

    Solve crypto square

--- crypto-square/crypto_square.py
+++ crypto-square/crypto_square.py
@@ -1,15 +1,18 @@
+"""A simple crypto square encoder."""
 import math
 import re
 
 
 def cipher_text(plain_text: str) -> None:
+    """Encode."""
+    if not plain_text:
+        return ""
     pattern = re.compile(r"[\W_]+", re.UNICODE)
     clean_text = pattern.sub("", plain_text.lower())
-    cipher_length = math.ceil(math.sqrt(len(clean_text)))
-    clean_text = clean_text + "*" * (cipher_length ** 2 - len(clean_text))
-    ciphered_text = " ".join(
-        [clean_text[i : i + 3] for i in range(0, len(clean_text), 3)]
+
+    sqr = math.sqrt(len(clean_text))
+    rows = math.ceil(sqr)
+    columns = round(sqr)
+    return " ".join(
+        f"{row:<{columns}}" for row in (clean_text[i::rows] for i in range(rows))
     )
-    # if len(clean_text) < cipher_length**2:
-    print(ciphered_text)
-    return clean_text
