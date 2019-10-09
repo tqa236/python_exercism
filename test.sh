e473957ed6ea6a793fe27413f1875c5aaf49fb48
TQA <trinhquocanh94@gmail.com>
2019-10-09 12:32:13 +0200

    Ignore unsolved exercises

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # find . -iname "*.py" | grep "test" | xargs pytest --cov
-pytest --cov --ignore=minesweeper --ignore=wordy --ignore=ocr-numbers --ignore=poker --ignore=rectangles --ignore=variable-length-quantity --ignore=scale-generator --ignore=connect --ignore=go-counting --ignore=two-bucket --ignore=react --ignore=binary-search-tree --ignore=change --ignore=knapsack --ignore=linked-list  --ignore=bowling --ignore=forth --ignore=grep --ignore=ledger --ignore=pov --ignore=satellite --ignore=transpose --ignore=world-search --ignore=zipper
+pytest --cov --ignore=minesweeper --ignore=wordy --ignore=ocr-numbers --ignore=poker --ignore=rectangles --ignore=variable-length-quantity --ignore=scale-generator --ignore=connect --ignore=go-counting --ignore=two-bucket --ignore=react --ignore=change --ignore=knapsack --ignore=linked-list  --ignore=bowling --ignore=forth --ignore=grep --ignore=ledger --ignore=pov --ignore=satellite --ignore=transpose --ignore=world-search --ignore=zipper --ignore=queen-attack
