5c318c963d4895d2fd895ad5dc11174a6da1e30b
TQA <trinhquocanh94@gmail.com>
2019-10-16 20:11:06 +0200

    Solve wordy

--- linked-list/linked_list.py
+++ linked-list/linked_list.py
@@ -1,6 +1,8 @@
 class Node(object):
     def __init__(self, value, succeeding=None, previous=None):
-        pass
+        self.value = value
+        self.succeeding = succeeding
+        self.previous = previous
 
 
 class LinkedList(object):
--- wordy/wordy.py
+++ wordy/wordy.py
@@ -1,2 +1,28 @@
+"""Parse and evaluate simple math word problems."""
+from operator import add, mul, sub
+from operator import truediv as div
+
+
 def calculate(question: str) -> None:
-    pass
+    """Parse and evaluate simple math word problems."""
+    elements = (
+        question.strip("?")
+        .replace("plus", "+")
+        .replace("minus", "-")
+        .replace("divided by", "/")
+        .replace("multiplied by", "*")
+        .split()
+    )
+    if (elements[0], elements[1]) != ("What", "is") or len(elements) % 2 == 0:
+        raise ValueError("Not a math problem.")
+    operator_dict = {"+": add, "-": sub, "/": div, "*": mul}
+    if elements[2].lstrip("-").isdigit():
+        result = int(elements[2])
+    else:
+        raise ValueError("No continuous operands")
+    for operator, operand in zip(elements[3::2], elements[4::2]):
+        try:
+            result = operator_dict[operator](result, int(operand))
+        except Exception as ex:
+            raise ValueError(f"Not a math problem: {ex}")
+    return result
