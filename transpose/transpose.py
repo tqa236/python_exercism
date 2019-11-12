05cc587421cd3607dcee8d46e5c1ad66582c5195
TQA <trinhquocanh94@gmail.com>
2019-11-13 00:48:50 +0100

    Solve transpose

--- transpose/transpose.py
+++ transpose/transpose.py
@@ -1,2 +1,10 @@
+"""Transpose a matrix."""
+import itertools
+
+
 def transpose(lines: str) -> None:
-    pass
+    """Transpose a matrix."""
+    transpose_matrix = itertools.zip_longest(*lines.splitlines(), fillvalue="\n")
+    return "\n".join(
+        ["".join(line).rstrip("\n").replace("\n", " ") for line in transpose_matrix]
+    )
