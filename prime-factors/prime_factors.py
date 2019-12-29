ef1f07c0774e1bf208c512df0c10e6753ef80fe3
TQA <trinhquocanh94@gmail.com>
2019-12-29 21:38:52 +0100

    Update exercise

--- prime-factors/prime_factors.py
+++ prime-factors/prime_factors.py
@@ -3,17 +3,17 @@ import math
 from typing import List, Union
 
 
-def prime_factors(natural_number: int) -> Union[List[Union[int, float]], List[int]]:
+def factors(natural_number: int) -> Union[List[Union[int, float]], List[int]]:
     """Factorize a number."""
     i = 2
-    factors = []
+    prime_factors = []
     while natural_number % 2 == 0:
-        factors.append(2)
+        prime_factors.append(2)
         natural_number = natural_number / 2
     for i in range(3, int(math.sqrt(natural_number)) + 1, 2):
         while natural_number % i == 0:
-            factors.append(i)
+            prime_factors.append(i)
             natural_number = natural_number / i
     if natural_number > 2:
-        factors.append(natural_number)
-    return factors
+        prime_factors.append(natural_number)
+    return prime_factors
