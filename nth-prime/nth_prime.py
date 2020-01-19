c2938f00bbf0553025bbd31e813de1d361411c83
TQA <trinhquocanh94@gmail.com>
2020-01-19 12:26:30 +0100

    Solve nth prime

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -1,2 +1,15 @@
-def prime(number: int) -> None:
-    pass
+"""Find the nth prime."""
+from math import log
+
+
+def prime(number: int) -> int:
+    """Find the nth prime."""
+    if number == 0:
+        raise ValueError("Input must be a positive integer.")
+    upper_bound = 10 + (number + 1) * int(log(number + 1) + log(log(number + 1)))
+    prime_list = range(2, upper_bound)
+    for i in range(int(number ** 0.5) + 1):
+        prime_list = list(
+            filter(lambda x: x % prime_list[i] != 0 or x == prime_list[i], prime_list)
+        )
+    return prime_list[number - 1]
