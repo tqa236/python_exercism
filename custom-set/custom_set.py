a9bc2a0caf7566579d12d47750d2ea2d0bfde7f1
TQA <trinhquocanh94@gmail.com>
2020-01-19 12:45:01 +0100

    -m

--- custom-set/custom_set.py
+++ custom-set/custom_set.py
@@ -1,33 +1,37 @@
+"""A lazy custom set implementation."""
 from typing import List, Optional
 
 
 class CustomSet:
+    """A lazy custom set implementation."""
+
     def __init__(self, elements: List[int] = []) -> None:
-        pass
+        self.elements = set(elements)
+        print(self.elements)
 
     def isempty(self) -> None:
-        pass
+        return not self.elements
 
     def __contains__(self, element: int) -> None:
-        pass
+        return element in self.elements
 
     def issubset(self, other: "CustomSet") -> None:
-        pass
+        return self.elements.issubset(other.elements)
 
     def isdisjoint(self, other: "CustomSet") -> None:
-        pass
+        return self.elements.isdisjoint(other.elements)
 
     def __eq__(self, other: Optional["CustomSet"]) -> None:
-        pass
+        return self.elements == other.elements
 
     def add(self, element: int) -> None:
-        pass
+        self.elements.add(element)
 
     def intersection(self, other: "CustomSet") -> None:
-        pass
+        return CustomSet(self.elements.intersection(other.elements))
 
     def __sub__(self, other: "CustomSet") -> None:
-        pass
+        return CustomSet(self.elements.difference(other.elements))
 
     def __add__(self, other: "CustomSet") -> None:
-        pass
+        return CustomSet(self.elements | other.elements)
