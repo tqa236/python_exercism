447a66e00653ecec545f0bd2a9c65690accd718f
TQA <trinhquocanh94@gmail.com>
2020-03-08 19:16:36 +0100

    Solve satellite

--- satellite/satellite.py
+++ satellite/satellite.py
@@ -1,7 +1,25 @@
+"""Construct a tree from the preoder and inorder traversals."""
 from typing import List
 
 
 def tree_from_traversals(preorder: List[str], inorder: List[str]) -> None:
+    """Construct a tree from the preoder and inorder traversals."""
     if len(preorder) != len(inorder):
         raise ValueError("Wrong tree")
-    return []
+    if len(preorder) != len(set(preorder)):
+        raise ValueError("Repeated item")
+    if not preorder:
+        return {}
+    root = preorder[0]
+    root_index = inorder.index(root)
+
+    left_inorder = inorder[:root_index]
+    right_inorder = inorder[root_index + 1 :]
+
+    left_preoder = preorder[1 : len(left_inorder) + 1]
+    right_preoder = preorder[len(left_inorder) + 1 :]
+
+    left_tree = tree_from_traversals(left_preoder, left_inorder)
+    right_tree = tree_from_traversals(right_preoder, right_inorder)
+
+    return {"v": root, "l": left_tree, "r": right_tree}
--- word-search/word_search.py
+++ word-search/word_search.py
@@ -1,18 +1,28 @@
+import re
 from typing import List
 
 
 class Point(object):
-    def __init__(self, x: int, y: int) -> None:
-        self.x = None
-        self.y = None
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
 
 class WordSearch(object):
-    def __init__(self, puzzle: List[str]) -> None:
-        pass
+    def __init__(self, puzzle: List[str]):
+        self.puzzle = puzzle
 
-    def search(self, word: str) -> None:
-        pass
+    def search(self, word: str):
+        result = [
+            (position.start(), index)
+            for index, row in enumerate(self.puzzle)
+            for position in re.finditer(word, row)
+        ]
+        if not result:
+            return None
+        print(result)
+        print([Point(point[0], point[1]) for point in result])
+        return [Point(x, y) for x, y in result]
