e205b15c996f6ae83ef9667a45e60ee4264edc01
TQA <trinhquocanh94@gmail.com>
2020-03-08 21:02:50 +0100

    Solve Diffie Hellman

--- diffie-hellman/diffie_hellman.py
+++ diffie-hellman/diffie_hellman.py
@@ -1,31 +1,14 @@
-from typing import Optional
-
-# import random
-
-
-def all_primes(start, end):
-    return list(
-        sorted(
-            set(range(start, end + 1)).difference(
-                set(
-                    (p * f)
-                    for p in range(2, int(end ** 0.5) + 2)
-                    for f in range(2, (end // p) + 1)
-                )
-            )
-        )
-    )
+"""Generate public and private keys."""
+import random
 
 
-def private_key(p: int) -> None:
-    return 2
-    # prime_list = all_primes(2, p - 1)
-    # return random.choice(prime_list)
+def private_key(p: int) -> int:
+    return random.randint(2, p - 1)
 
 
-def public_key(p: int, g: int, private: Optional[int]) -> None:
-    pass
+def public_key(p: int, g: int, private: int) -> int:
+    return (g ** private) % p
 
 
-def secret(p: int, public: Optional[int], private: Optional[int]) -> None:
-    pass
+def secret(p: int, public: int, private: int) -> int:
+    return (public ** private) % p
