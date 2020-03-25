88cbd086bb988c35bb445942293dce41f1643556
TQA <trinhquocanh94@gmail.com>
2020-03-25 22:52:26 +0100

    Update word count

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -6,4 +6,5 @@ from collections import Counter
 def count_words(phrase):
     """Count the number of words in a phrase."""
     phrase = re.sub(r"(\' | \')", " ", phrase.lower())
+    phrase = re.sub(r"(^(\')+|(\')+$)", "", phrase)
     return Counter(item for item in re.split("[^a-zA-Z0-9']", phrase) if item)
