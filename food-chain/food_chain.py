49685c9c2f94b3723ce58c4e031f205633aed118
TQA <trinhquocanh94@gmail.com>
2020-03-29 13:32:44 +0200

    Solve food chain

--- food-chain/food_chain.py
+++ food-chain/food_chain.py
@@ -1,17 +1,41 @@
-ANIMAL_ACTION = [
-    ("fly", "I don't know why she swallowed the fly."),
+"""Refactor the food chain song."""
+
+ANIMAL_ACTIONS = [
+    ("fly", "I don't know why she swallowed the fly. Perhaps she'll die."),
     ("spider", "It wriggled and jiggled and tickled inside her."),
+    ("bird", "How absurd to swallow a bird!"),
+    ("cat", "Imagine that, to swallow a cat!"),
+    ("dog", "What a hog, to swallow a dog!"),
+    ("goat", "Just opened her throat and swallowed a goat!"),
+    ("cow", "I don't know how she swallowed a cow!"),
+    ("horse", "She's dead, of course!"),
 ]
 
 
 def recite(start_verse: int, end_verse: int) -> None:
-    song = [
-        [
-            f"I know an old lady who swallowed a {ANIMAL_ACTION[i][0]}.",
-            f"{ANIMAL_ACTION[i][1]}",
-        ]
-        for i in range(end_verse - 1, -1, -1)
-    ]
-    song[-1][-1] = song[-1][-1] + " Perhaps she'll die."
-    print(song)
-    return [sentence for verse in song for sentence in verse]
+    song = []
+    for i in range(start_verse, end_verse):
+        song = song + recite_one_verse(i)
+        song.append("")
+    song = song + recite_one_verse(end_verse)
+    return song
+
+
+def recite_one_verse(verse: int):
+    lyrics = []
+    for i in range(verse - 1, -1, -1):
+        if i == verse - 1:
+            lyrics.append(f"I know an old lady who swallowed a {ANIMAL_ACTIONS[i][0]}.")
+        elif i == 1:
+            lyrics.append(
+                f"She swallowed the {ANIMAL_ACTIONS[i+1][0]} to catch the {ANIMAL_ACTIONS[i][0]} that wriggled and jiggled and tickled inside her."
+            )
+        else:
+            lyrics.append(
+                f"She swallowed the {ANIMAL_ACTIONS[i+1][0]} to catch the {ANIMAL_ACTIONS[i][0]}."
+            )
+        if i == verse - 1 or i == 0:
+            lyrics.append(f"{ANIMAL_ACTIONS[i][1]}")
+        if i == len(ANIMAL_ACTIONS) - 1:
+            break
+    return lyrics
