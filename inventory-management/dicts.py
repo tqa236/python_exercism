1a92e59a975dcb2f613061638a22ea0ab623fa88
tqa <trinhquocanh94@gmail.com>
2022-01-25 16:47:51 +0100

    Add dict

---
+++ inventory-management/dicts.py
@@ -0,0 +1,57 @@
+from collections import Counter
+
+
+def create_inventory(items):
+    """
+
+    :param items: list - list of items to create an inventory from.
+    :return:  dict - the inventory dictionary.
+    """
+
+    return dict(Counter(items))
+
+
+def add_items(inventory, items):
+    """
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return:  dict - the inventory dictionary update with the new items.
+    """
+
+    return dict(Counter(inventory) + Counter(items))
+
+
+def decrement_items(inventory, items):
+    """
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return:  dict - updated inventory dictionary with items decremented.
+    """
+    for key, value in Counter(items).items():
+        inventory[key] = max(inventory.get(key, 0) - value, 0)
+
+    return inventory
+
+
+def remove_item(inventory, item):
+    """
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return:  dict - updated inventory dictionary with item removed.
+    """
+
+    if item in inventory:
+        del inventory[item]
+    return inventory
+
+
+def list_inventory(inventory):
+    """
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    return list((Counter(inventory) - Counter()).items())
