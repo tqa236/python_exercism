ff56614ac30048269e37cea0ec330c2121fb7cd3
tqa <trinhquocanh94@gmail.com>
2023-07-12 10:37:56 +0200

    download ellens alien games

---
+++ ellens-alien-game/classes.py
@@ -0,0 +1,25 @@
+"""Solution to Ellen's Alien Game exercise."""
+
+
+class Alien:
+    """Create an Alien object with location x_coordinate and y_coordinate.
+
+    Attributes
+    ----------
+    (class)total_aliens_created: int
+    x_coordinate: int - Position on the x-axis.
+    y_coordinate: int - Position on the y-axis.
+    health: int - Amount of health points.
+
+    Methods
+    -------
+    hit(): Decrement Alien health by one point.
+    is_alive(): Return a boolean for if Alien is alive (if health is > 0).
+    teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
+    collision_detection(other): Implementation TBD.
+    """
+
+    pass
+
+
+#TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
