b07cd792d6d6fec0aebfec8ac090937f2ddcf338
tqa <trinhquocanh94@gmail.com>
2023-07-12 10:51:26 +0200

    format ellens alien games

--- ellens-alien-game/classes.py
+++ ellens-alien-game/classes.py
@@ -19,7 +19,28 @@ class Alien:
     collision_detection(other): Implementation TBD.
     """
 
-    pass
+    total_aliens_created: int = 0
+
+    def __init__(self, x_coordinate: int, y_coordinate: int):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.health = 3
+        total_aliens_created += 1
+
+    def hit(self):
+        self.health -= 1
+
+    def is_alive(self):
+        self.health > 0
+
+    def teleport(self, new_x_coordinate: int, new_y_coordinate: int):
+        self.x_coordinate = new_x_coordinate
+        self.y_coordinate = new_y_coordinate
+
+    def collision_detection(self, other):
+        raise NotImplementedError
 
 
 # TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+def new_aliens_collection(position_data):
+    return [Alien(position[0], position[1]) for position in position_data]
