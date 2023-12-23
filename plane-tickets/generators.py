0cf4d164702b05d7a59a1b6e74e735ff2fd0a1e4
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-23 20:12:49 +0100

    solve plane ticket

---
+++ plane-tickets/generators.py
@@ -0,0 +1,66 @@
+"""Functions to automate Conda airlines ticketing system."""
+import itertools
+
+
+def generate_seat_letters(number):
+    """Generate a series of letters for airline seats.
+
+    :param number: int - total number of seat letters to be generated.
+    :return: generator - generator that yields seat letters.
+
+    Seat letters are generated from A to D.
+    After D it should start again with A.
+
+    Example: A, B, C, D
+
+    """
+    seat_letters = ["A", "B", "C", "D"] * (number // 4 + 1)
+    return (seat_letter for seat_letter in seat_letters[:number])
+
+
+def generate_seats(number):
+    """Generate a series of identifiers for airline seats.
+
+    :param number: int - total number of seats to be generated.
+    :return: generator - generator that yields seat numbers.
+
+    A seat number consists of the row number and the seat letter.
+
+    There is no row 13.
+    Each row has 4 seats.
+
+    Seats should be sorted from low to high.
+
+    Example: 3C, 3D, 4A, 4B
+
+    """
+    numbers = [i // 4 + 1 for i in range(number + 5) if i // 4 + 1 != 13][:number]
+    seat_letters = generate_seat_letters(number)
+    return (
+        str(number) + seat_letter for number, seat_letter in zip(numbers, seat_letters)
+    )
+
+
+def assign_seats(passengers):
+    """Assign seats to passengers.
+
+    :param passengers: list[str] - a list of strings containing names of passengers.
+    :return: dict - with the names of the passengers as keys and seat numbers as values.
+
+    Example output: {"Adele": "1A", "Björk": "1B"}
+
+    """
+
+    seats = generate_seats(len(passengers))
+    return {passenger: seat for passenger, seat in zip(passengers, seats)}
+
+
+def generate_codes(seat_numbers, flight_id):
+    """Generate codes for a ticket.
+
+    :param seat_numbers: list[str] - list of seat numbers.
+    :param flight_id: str - string containing the flight identifier.
+    :return: generator - generator that yields 12 character long ticket codes.
+
+    """
+    return ((seat_number + flight_id).ljust(12, "0") for seat_number in seat_numbers)
