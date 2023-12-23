9a160fa13955997758cd452fa0fe526f51e9e468
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-23 21:06:48 +0100

    solve triangle

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -4,21 +4,21 @@
 from typing import List, Union
 
 
-def is_triangle(sides: Union[List[float], List[int]]) -> bool:
+def triangle(sides: Union[List[float], List[int]]) -> bool:
     """Check if the sides satisfied the triangle inequility."""
     return min(sides) > 0 and sum(sides) > 2 * max(sides)
 
 
-def is_equilateral(sides: Union[List[float], List[int]]) -> bool:
+def equilateral(sides: Union[List[float], List[int]]) -> bool:
     """Check if the triangle is equilateral."""
-    return is_triangle(sides) and min(sides) == max(sides)
+    return triangle(sides) and min(sides) == max(sides)
 
 
-def is_isosceles(sides: Union[List[float], List[int]]) -> bool:
+def isosceles(sides: Union[List[float], List[int]]) -> bool:
     """Check if the triangle is isosceles."""
-    return is_triangle(sides) and not is_scalene(sides)
+    return triangle(sides) and not scalene(sides)
 
 
-def is_scalene(sides: Union[List[float], List[int]]) -> bool:
+def scalene(sides: Union[List[float], List[int]]) -> bool:
     """Check if the triangle is scalene."""
-    return is_triangle(sides) and (len(set(sides)) == 3)
+    return triangle(sides) and (len(set(sides)) == 3)
