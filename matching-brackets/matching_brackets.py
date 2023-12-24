9495a6c6931dc7fb6f1e9240fc23cf733035f654
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-24 11:33:02 +0100

    solve matching brackets

---
+++ matching-brackets/matching_brackets.py
@@ -0,0 +1,19 @@
+"""Check if a string has correct bracket order or not."""
+import re
+
+
+def is_paired(input_string: str) -> bool:
+    """Check if a string has correct bracket order or not."""
+    stack = []
+    brackets = re.sub(r"[^\(\)\[\]\{\}]", "", input_string)
+    bracket_pairs = {"{": "}", "(": ")", "[": "]"}
+    for i in brackets:
+        if i in bracket_pairs:
+            stack.append(bracket_pairs[i])
+        elif not stack:
+            return False
+        elif i == stack[-1]:
+            stack.pop()
+        else:
+            return False
+    return not stack
