4f99f8379a7fb4c0a61b92cb7b03fe3ac6af4720
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-25 14:11:12 +0100

    update pascal triangle

--- pascals-triangle/pascals_triangle.py
+++ pascals-triangle/pascals_triangle.py
@@ -1,2 +1,18 @@
+from functools import cache
+from copy import deepcopy
+
+
+@cache
 def rows(row_count):
-    pass
+    if row_count < 0:
+        raise ValueError("number of rows is negative")
+    if row_count == 0:
+        return []
+    if row_count == 1:
+        return [[1]]
+    base_triangle = deepcopy(rows(row_count - 1))
+    last_line = [
+        i + j for i, j in zip([0] + base_triangle[-1], base_triangle[-1] + [0])
+    ]
+    base_triangle.append(last_line)
+    return base_triangle
