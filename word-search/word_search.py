583415d7d620bd9951c6363f39ef5a100c4b59d5
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-28 11:45:20 +0100

    update word search

--- word-search/word_search.py
+++ word-search/word_search.py
@@ -1,8 +1,23 @@
-import re
-from typing import List, Optional
+from collections import defaultdict
 
 
-class Point(object):
+def groups(data, func):
+    grouping = defaultdict(list)
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            grouping[func(x, y)].append(data[y][x])
+    return list(map(grouping.get, sorted(grouping)))
+
+
+def search_list(puzzle, word):
+    for j, row in enumerate(puzzle):
+        if word in row:
+            i = row.index(word)
+            return i, j
+    return None, None
+
+
+class Point:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
@@ -11,18 +26,38 @@ class Point(object):
         return self.x == other.x and self.y == other.y
 
 
-class WordSearch(object):
-    def __init__(self, puzzle: List[str]) -> None:
+class WordSearch:
+    def __init__(self, puzzle):
         self.puzzle = puzzle
 
-    def search(self, word: str) -> Optional[List[Point]]:
-        result = [
-            (position.start(), index)
-            for index, row in enumerate(self.puzzle)
-            for position in re.finditer(word, row)
+    def search(self, word):
+        p1, p2 = None, None
+        width, height = len(self.puzzle[0]) - 1, len(self.puzzle) - 1
+        directions = (
+            (1, 0),
+            (-1, 0),
+            (0, 1),
+            (0, -1),
+            (-1, -1),
+            (1, -1),
+            (-1, 1),
+            (1, 1),
+        )
+        points = [
+            (x, y)
+            for x in range(width + 1)
+            for y in range(height + 1)
+            if self.puzzle[y][x] == word[0]
         ]
-        if not result:
-            return None
-        print(result)
-        print([Point(point[0], point[1]) for point in result])
-        return [Point(x, y) for x, y in result]
+        for x, y in points:
+            p1 = Point(x, y)
+            for direction in directions:
+                line = []
+                x2, y2 = x, y
+                while 0 <= x2 <= width and 0 <= y2 <= height:
+                    line.append(self.puzzle[y2][x2])
+                    if word == "".join(line):
+                        p2 = Point(x2, y2)
+                        return p1, p2
+                    x2 += direction[0]
+                    y2 += direction[1]
